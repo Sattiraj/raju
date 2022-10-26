11706e9a56de283205ab2092b3915db802665ee7
sattiraj <sattiraju.yarramsetti2000@gmail.com>
2022-10-26 12:42:09 +0530

    first commit

--- Demo3.java
+++ Demo3.java
@@ -6,21 +6,20 @@ class demo
 		System.out.println();
 		System.out.println();
 		for(int i=0;i<n;i++)
+		{
+			System.out.print("      ");
+			for(int j=0;j<n;j++)
 			{
-				System.out.print("      ");
-				for(int j=0;j<n;j++)
+				if(i==0||i==n-1||j==0&&i<n-1||j==n-1&&i<n-1||i+j<=(n-1)/2||j-i>=(n-1)/2)
 					{
-						if(i==0||i==n-1||j==0&&i<n-1||j==n-1&&i<n-1||i+j<=(n-1)/2||j-i>=(n-1)/2)
-							{
-								System.out.print("*");
-							}
-						else
-							{
-								System.out.print(" ");
-							}
+						System.out.print("*");
 					}
-				System.out.println();
+					else
+					{
+						System.out.print(" ");
+					}
 			}
+			System.out.println();
+		}
 	}
 }
-					
--- Demo4.java
+++ Demo4.java
@@ -4,21 +4,21 @@ class demo
 	{
 		int n=20;
 		for(int i=0;i<n;i++)
+		{
+			System.out.print("      ");
+			for(int j=0;j<n;j++)
 			{
-				System.out.print("      ");
-				for(int j=0;j<n;j++)
-					{
-						if(i-j>=(n-1)/2||i+j>=(n-1)+(n-1)/2)
-							{
-								System.out.print("*");
-							}
-						else
-							{
-								System.out.print(" ");
-							}
-					}
-				System.out.println();
+				if(i-j>=(n-1)/2||i+j>=(n-1)+(n-1)/2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+     				System.out.print(" ");
+				}
 			}
+			System.out.println();
+		}
 	}
 }
 					
